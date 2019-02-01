1c80e6e7a234c6b49c8df66b9e2f7b21cb062860
Lucas Cherkewski <lucas@ecustom.ca>
2019-01-31 20:01:31 -0500

    remove unused query

--- db.R
+++ db.R
@@ -7,25 +7,6 @@ library(dbplyr)
 con <- DBI::dbConnect(drv = dbDriver("PostgreSQL"), host = "localhost", dbname = "openparliament")
 
 db_ethi_statements <- tbl(con, "hansards_statement") %>%
-  filter(str_detect(urlcache, "/committees/ethics/42-1/")) %>%
-  select(
-    time,
-    sequence,
-    id,
-    slug,
-    member_id,
-    who_en,
-    who_context_en,
-    politician_id,
-    content_en,
-    statement_type,
-    procedural,
-    written_question,
-    wordcount,
-    urlcache
-  )
-
-db_ethi_statements <- tbl(con, "hansards_statement") %>%
   left_join(tbl(con, "committees_committeemeeting"), by = c("document_id" = "evidence_id")) %>%
   filter(committee_id == 56 & session_id == "42-1") %>%
   select(
