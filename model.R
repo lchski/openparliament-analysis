4ae653828f967bf2023306661e182246b2b2c666
Lucas Cherkewski <lucas@ecustom.ca>
2019-02-08 19:20:27 -0500

    clean up a bit

--- model.R
+++ model.R
@@ -25,48 +25,31 @@ speeches_lda <- LDA(speeches_dtm, k = 10, control = list(seed = 1))
 
 speeches_lda_td <- tidy(speeches_lda)
 
-top_terms <- speeches_lda_td %>%
+## how to make a "grouped" facet: https://drsimonj.svbtle.com/ordering-categories-within-ggplot2-facets
+top_terms_ordered <- speeches_lda_td %>%
   group_by(topic) %>%
   top_n(5, beta) %>%
   ungroup() %>%
-  arrange(topic, -beta)
-
-reorder <- top_terms %>%
-  mutate(term = reorder(term, -beta))
-
-reorder_grouped <- top_terms %>%
-  group_by(topic) %>%
-  mutate(term = reorder(term, -beta)) %>%
-  ungroup()
-
-reorder_grouped_2 <- top_terms %>%
   arrange(topic, beta) %>%
   mutate(order = row_number())
 
-## how to make a "grouped" facet: https://drsimonj.svbtle.com/ordering-categories-within-ggplot2-facets
-reorder_grouped_2 %>% ggplot(aes(order, beta)) +
+top_terms_ordered %>% ggplot(aes(order, beta)) +
   geom_bar(stat = "identity") +
   facet_wrap(~ topic, scales = "free_y") +
   xlab("Term") +
   ylab("Beta (term weight)") +
   theme_bw() +
   scale_x_continuous(
-    breaks = reorder_grouped_2$order,
-    labels = reorder_grouped_2$term,
+    breaks = top_terms_ordered$order,
+    labels = top_terms_ordered$term,
     expand = c(0,0)
   ) +
   coord_flip()
 
-top_terms %>%
-  mutate(term = reorder(topic, term, -beta)) %>%
-  ggplot(aes(term, beta)) +
-  geom_bar(stat = "identity") +
-  facet_wrap(~ topic, scales = "free_y") +
-  theme(axis.text.x = element_text(size = 10, angle = 90, hjust = 1)) +
-  coord_flip()
-
-top_terms_20 <- speeches_lda_td %>%
+terms_by_topic <- speeches_lda_td %>%
   group_by(topic) %>%
   top_n(20, beta) %>%
   ungroup() %>%
-  arrange(topic, -beta)
+  arrange(topic, -beta) %>%
+  group_by(topic) %>%
+  summarize(terms = paste(term, collapse=" "))
